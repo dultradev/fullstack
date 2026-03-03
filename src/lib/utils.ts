cf413692f21f7c5aea29a953fa1d44339695465d
dultradev <rian.albert.dultra@gmail.com>
2026-03-02 23:40:29 -0300

    DultraDevPortfolio

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,6 @@
-export function formatMonthYear(value: string, locale: string): string {
-  const [year, month] = value.split("-").map(Number);
-  const date = new Date(Date.UTC(year, month - 1, 1));
-  return new Intl.DateTimeFormat(locale, {
-    year: "numeric",
-    month: "short",
-    timeZone: "UTC"
-  }).format(date);
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
