2749f975cadfecf2e614d88aa19ce368ecd37477
dultradev <rian.albert.dultra@gmail.com>
2026-03-02 23:53:53 -0300

    Vercel Adjustments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatMonthYear(value: string, locale: string): string {
+  const [year, month] = value.split("-").map(Number);
+  const date = new Date(Date.UTC(year, month - 1, 1));
+
+  return new Intl.DateTimeFormat(locale, {
+    year: "numeric",
+    month: "short",
+    timeZone: "UTC"
+  }).format(date);
+}
